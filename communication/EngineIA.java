d408da14998dc34ce4eb0e76d1187e4242cff4bd
staamp <nicoce25@hotmail.fr>
2020-03-30 16:51:36 +0200

    debug

--- communication/EngineIA.java
+++ communication/EngineIA.java
@@ -6,42 +6,36 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EngineIA {
-  
-    
-  
-  
-    public static void main (String [] args) {
-        if(args.length != 2) {
+	public static void main (String[] args) {
+
+		int port = 0;
+		String addr = "";
+		
+		if(args.length != 2) {
 			System.out.println("Usage : adresse port");
-        }
-        int port = Integer.parseInt(args[1]);
-        String addr = args[0];
-        try{
-
-            Socket sock = new Socket(addr,port);
-            System.out.println("SOCKET : " + sock);
-            
-            DataInputStream DIS = new DataInputStream(sock.getInputStream());
+		}
+		addr = args[0];
+		port = Integer.parseInt(args[1]);
+		try{
+			Socket sock = new Socket(addr,port);
+			DataInputStream DIS = new DataInputStream(sock.getInputStream());
 			DataOutputStream DOS = new DataOutputStream(sock.getOutputStream());
-            
-                System.out.println(sock.isClosed());
-                int result = 0;
-                while(DIS.available() > 0) {
-                    result = DIS.readInt();
-                }
-                
-                
-                System.out.print(" RESULTE"+result);
-
-                DOS.write(result);
-                
-                System.out.print(" RESULTE"+result);
-               
-            sock.close();
-
-        } catch (Exception e) {
+			
+			int arret = 6;
+			int result = 0;
+				
+				result = DIS.readInt();
+				System.out.println("RECU " + result);
+				DOS.writeInt(arret);
+				
+				//result = DIS.readInt();
+				//System.out.println("RECU " + result);
+				
+				DIS.close();
+				DOS.close();
+				sock.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-    }
+	}
 }
