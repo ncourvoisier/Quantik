cf23aed1105b5a2405edce72a471e3eda9c62ab1
staamp <nicoce25@hotmail.fr>
2020-04-07 17:26:49 +0200

    ajout coup (je suis encore un tricheur donc c'est pas finit)

--- communication/player.c
+++ communication/player.c
@@ -6,6 +6,81 @@ void printHelp(char* name) {
     printf("usage : %s IPserver port name color(W/B) portIA\n", name);
 }
 
+
+TLg intToLigne(int x) {
+	switch(x) {
+        case 0 : 
+            return UN;
+        case 1 :
+            return DEUX;
+        case 2 :
+            return TROIS;
+        default :
+            return QUATRE;
+    } 
+}
+
+TCol intToColonne(int y){
+	switch(y) {
+        case 0 : 
+            return A;
+        case 1 :
+            return B;
+        case 2 :
+            return C;
+        default :
+            return D;
+    } 
+}
+
+TTypePion intToPion(int p) {
+	switch(p) {
+        case 0 : 
+            return CYLINDRE;
+        case 1 :
+            return PAVE;
+        case 2 :
+            return SPHERE;
+        default :
+            return TETRAEDRE;
+    } 
+}
+
+TCase positionToTCase(int x, int y) {
+	TLg ligne = intToLigne(x);
+	TCol colonne = intToColonne(y);
+	
+	TCase tcase;
+	
+	tcase.l = ligne;
+	tcase.c = colonne;
+	
+	return tcase;
+}
+
+TPion pionToTPion(int p, TCoul couleurPion) {
+	TTypePion tp = intToPion(p);
+	
+	TPion tpion;
+	
+	tpion.coulPion = couleurPion;
+	tpion.typePion = tp;
+	
+	return tpion;
+}
+
+TCoupReq requeteCoup(TIdReq idRequest, int numPartie, bool estBloque, TPion pion, TCase positionPion, TPropCoup propCoup) {
+	TCoupReq cr;
+	cr.idRequest = idRequest;
+	cr.numPartie = numPartie;
+	cr.estBloque = estBloque;
+	cr.pion = pion;
+	cr.posPion = positionPion;
+	cr.propCoup = propCoup;
+	return cr;
+}
+
+
 int connectionIA(int port) {
   printf("CONNECTION IA");
   struct sockaddr_in addClient;
@@ -87,8 +162,10 @@ int main (int argc, char** argv) {
         return -2;
     }
     
+    gameRequest.idReq = PARTIE;
+    
     strcpy(gameRequest.nomJoueur, playerName);
-/*
+
     err = send(sock, &gameRequest, sizeof(TPartieReq),0);
     if (err <= 0) {
         perror("(player) send error with the game request\n");
@@ -107,7 +184,7 @@ int main (int argc, char** argv) {
 
     switch(gameResponse.err) {
         case ERR_OK : 
-            printf("You play against : %s", gameResponse.nomAdvers);
+            printf("You play against : %s\n", gameResponse.nomAdvers);
             break;
         case ERR_PARTIE :
             printf("Couldn't log into the game, invalid game request\n");
@@ -121,22 +198,22 @@ int main (int argc, char** argv) {
 
     if (gameResponse.validCoulPion) {
         if (gameRequest.coulPion == BLANC) {
-            printf("Color not available, you will with black.\n");
             gameRequest.coulPion = NOIR;
+            printf("Color not available, you will play with black (%d).\n",gameRequest.coulPion);
         } else {
-            printf("Color not available, you will with white.\n");
             gameRequest.coulPion = BLANC;
+        		printf("Color not available, you will play with white (%d).\n",gameRequest.coulPion);
         }
     } else {
        if (gameRequest.coulPion == BLANC) {
-            printf("You will with white.\n");
+            printf("You will play with white (%d).\n",gameRequest.coulPion);
         } else {
-            printf("You will with black.\n");
+            printf("You will play with black (%d).\n",gameRequest.coulPion);
         }
-    }*/
+    }
 
 
-    printf("The game is starded :\n");
+   /* printf("The game is starded :\n");
     int sockIA  = connectionIA(portIA); // Connexion a l'IA
       
     // Envoie au java
@@ -150,10 +227,64 @@ int main (int argc, char** argv) {
     int recu = recvIA(sockIA);
     if (recu == -1) {
     	return -1;
+    }  */
+    
+    int x = 1;
+    int y = 2;
+    int p = 1;
+    
+    TPion tpion = pionToTPion(p, gameRequest.coulPion);
+    TCase tcase = positionToTCase(x, y);
+		TPropCoup propCoup;
+		bool estBloque = false;
+    
+    printf("[Ligne %d colonne %d Pion %d Couleur %d]\n",tcase.l, tcase.c, tpion.typePion, tpion.coulPion);
+    TCoupReq coupReq = requeteCoup(COUP, 1, estBloque, tpion, tcase, propCoup);
+    TCoupRep coupResponse;
+   	
+   	err = send(sock, &coupReq, sizeof(TCoupReq),0);
+    if (err <= 0) {
+        perror("(player) send error during the last try\n");
+        shutdown(sock, SHUT_RDWR); close(sock);
+        return -4;
+    }
+   
+   	printf("Waiting the player to play...\n");
+
+    err = recv(sock, &coupResponse, sizeof(TCoupRep),0);
+    if (err <= 0) {
+        perror("(player) recv error with the game response\n");
+        shutdown(sock, SHUT_RDWR); close(sock);
+        return -5;
     }
     
-   
-    printf("\nRECU DU JAVA : %d\n", recu);
+    switch(coupResponse.err) {
+    	case ERR_OK :
+          break;
+      case ERR_PARTIE :
+          printf("Couldn't log into the game, invalid move\n");
+          break;
+      case ERR_TYP :
+          printf("Couldn't log into the game, move\n");
+          break;
+      default :
+          printf("default\n");
+    }
+    
+    switch(coupResponse.validCoup) {
+    	case VALID : 
+          printf("Your move is valid\n");
+          break;
+      case TIMEOUT :
+          printf("Your move isn't valid, you are over time to play.\n");
+          break;
+      case TRICHE :
+          printf("Your move isn't valid, CHEATER\n");
+          break;
+      default :
+          printf("default\n");
+    }
+    
 
     shutdown(sock, SHUT_RDWR); close(sock);
     return 0;
