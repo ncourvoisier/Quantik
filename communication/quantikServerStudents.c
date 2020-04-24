17f8aedc9d6591eba87e86ae4ed882c29aecabeb
Zagarwin <augustin.bordy@gmail.com>
2020-04-25 00:01:52 +0200

    timer mechanic + verifications + game loop

--- communication/quantikServerStudents.c
+++ communication/quantikServerStudents.c
@@ -3,21 +3,26 @@
 #include "protocolQuantik.h"
 
 #include <time.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define SEVERE_COLOR "\x1B[31m"
 #define RESET_COLOR "\x1B[0m"
+#define TIME_MAX 5000
 
 /*
     Structures definition
 */
 typedef struct PlayerData {
     int id;
+    int number; // playing order of the player in the current round
     struct PlayerData *opponent;
     char name[T_NOM];
     int socket;
     TCoul color;
     TValidCoul colorAccepted;
     int ready;
+    int score;
 } PlayerData;
 
 
@@ -34,6 +39,15 @@ int port = -1;
 */
 PlayerData players[2];
 int gameStarted = 0;
+int roundNumber = 1; // 1 for the first round, 2 for the second
+int playerNumber; //player currently playing
+int playerTimedOut = 0;
+
+/*
+    Timer datas
+*/
+pid_t clockPid;
+int timerInterrupted = 0;
 
 
 /*
@@ -42,6 +56,48 @@ int gameStarted = 0;
 int debug = 0;
 char logMessage[200];
 typedef enum { INFO, SEVERE, DEBUG } LogType;
+
+
+/*
+    Called in the timer process.
+    Interrupts the timer.
+*/
+void timerInterruptSignalHandler() {
+    timerInterrupted = 1;
+}
+
+
+void startClockTimer() {
+    struct timespec nano1, nano2;
+    nano1.tv_sec = 0;
+    nano1.tv_nsec = 1000000L; // 1 micro second
+    pid_t ppid = getpid();
+    clockPid = fork();
+    if (clockPid == 0) {
+        for (;;) {
+            for (int i = 0; i < TIME_MAX && !timerInterrupted; i++) {
+                nanosleep(&nano1, &nano2);
+            }
+            if (timerInterrupted) {
+                timerInterrupted = 0;
+            }
+            else {
+                kill(ppid, SIGUSR1); // The timer is out
+            }
+        }
+    }
+}
+
+
+void resetClockTimer() {
+    kill(clockPid, SIGUSR2);
+}
+
+
+void stopClockTimer() {
+    kill(clockPid, SIGKILL);
+    wait(NULL);
+}
 
 
 void printLog(LogType type) {
@@ -149,18 +205,103 @@ int sendGameResponse(PlayerData *player, TCodeRep code) {
 }
 
 
-int sendPlayingResponse(PlayerData *player, TCodeRep code) {
-    // TODO
+int sendPlayingResponse(TCodeRep code, TValCoup val, TPropCoup prop) {
+    TCoupRep playingResponse;
+    playingResponse.err = code;
+    playingResponse.validCoup = val;
+    playingResponse.propCoup = prop;
+    for (int i = 0; i < 2; i++) {
+        int err = send(players[i].socket, &playingResponse, sizeof(TCoupRep), 0);
+        if (err <= 0) {
+            // TODO
+        }
+    }
+}
+
+
+void launchRound(int number) {
+    initialiserPartie();
+    sprintf(logMessage, "Starting the round %i", number); printLog(INFO);
+    roundNumber = number;
+    playerNumber = 1;
+    if (roundNumber == 1) {
+        if (players[0].color == BLANC) {
+            players[0].number = 1;
+            players[1].number = 2;
+        }
+        else {
+            players[0].number = 2;
+            players[1].number = 1;
+        }
+    }
+    else {
+        if (players[0].color == BLANC) {
+            players[0].number = 2;
+            players[1].number = 1;
+        }
+        else {
+            players[0].number = 1;
+            players[1].number = 2;
+        }
+    }
 }
 
 
 void launchGame() {
-    sprintf(logMessage, "Starting the game"); printLog(INFO);
     for (int i = 0; i < 2; i++) {
         sendGameResponse(&players[i], ERR_OK); // TODO test ret
     }
+    if (timeout) {
+        startClockTimer();
+    }
     gameStarted = 1;
-    // Game initializing ...
+    launchRound(1);
+}
+
+
+void endGame() {
+    if (timeout) {
+        stopClockTimer();
+    }
+    // TODO closes ...
+}
+
+
+void endRound() {
+     if (roundNumber == 1) {
+         roundNumber = 2;
+         launchRound(2);
+     }
+     else {
+         endGame();
+     }
+}
+
+
+void endAction(PlayerData *player, TPropCoup moveProperty, TCoupReq playingRequest) {
+    if (timeout) {
+        resetClockTimer();
+    }
+
+    int err;
+
+    switch (moveProperty) {
+        case CONT:
+            err = send(player->opponent->socket, &playingRequest, sizeof(TCoupReq), 0);
+            if (err <= 0) {
+                // TODO
+            }
+            playerNumber = player->opponent->number;
+            return;
+        case GAGNE:
+            player->score++;
+            break;
+        case PERDU:
+            player->opponent->score++;
+            break;
+    }
+
+    endRound();
 }
 
 
@@ -246,6 +387,9 @@ int handleGameRequest(PlayerData *player) {
 
 /*
     Called for playing requests.
+    Error code:
+    1 -> invalid request type
+    2 -> invalid move
 */
 int handlePlayingRequest(PlayerData *player) {
     TCoupReq playingRequest;
@@ -255,6 +399,35 @@ int handlePlayingRequest(PlayerData *player) {
         sprintf(logMessage, "Error occured while receiving player request"); printLog(SEVERE);
         return 1;
     }
+
+    if (player->number != playerNumber) { // Not this player turn to play
+        return 1;
+    }
+
+    if (timeout && playerTimedOut) {
+        sendPlayingResponse(ERR_COUP, TIMEOUT, PERDU);
+        endAction(player, PERDU, playingRequest);
+        return 0;
+    }
+
+    TPropCoup moveProperty;
+    bool isValidMove;
+
+    if (valid) { // If validation is turned on
+        isValidMove = validationCoup(player->number, playingRequest, &moveProperty);
+    }
+    else {
+        isValidMove = true;
+        moveProperty = playingRequest.propCoup;
+    }
+
+    if (isValidMove) {
+        sendPlayingResponse(ERR_OK, VALID, moveProperty);
+        endAction(player, moveProperty, playingRequest);
+        return 0;
+    }
+
+    return 2;
 }
 
 
@@ -291,8 +464,21 @@ int handlePlayerAction(PlayerData *player) {
             }
         }
         else if (idRequest == COUP) {
-            handlePlayingRequest(player);
-            // Return ...
+            if (!gameStarted) {
+                errorCode = ERR_TYP;
+            }
+            else {
+                err = handlePlayingRequest(player);
+                if (err = 0) {
+                    return 0;
+                }
+                else if (err == 1) {
+                    errorCode = ERR_TYP;
+                }
+                else if (err == 2) {
+                    errorCode = ERR_COUP;
+                }
+            }
         }
         else {
             errorCode = ERR_TYP;
@@ -300,13 +486,24 @@ int handlePlayerAction(PlayerData *player) {
     }
 
     if (gameStarted) {
-        sendPlayingResponse(player, errorCode);
+        sendPlayingResponse(errorCode, TRICHE, PERDU);
     }
     else {
         sendGameResponse(player, errorCode);
     }
 
     return 0;
+}
+
+
+/*
+    Called in the main instance.
+    Handling the signal sent by the timer process when it ends.
+*/
+void timerOutSignalHandler() {
+    sprintf(logMessage, "Player %i timed out", playerNumber); printLog(INFO);
+    sendPlayingResponse(ERR_COUP, TIMEOUT, PERDU);
+    endRound();
 }
 
 
@@ -318,6 +515,9 @@ int main(int argc, char** argv) {
     int err,
         socketServer,
         maxSock;
+
+    signal(SIGUSR1, timerOutSignalHandler);
+    signal(SIGUSR2, timerInterruptSignalHandler);
 
     initPlayers();
 
@@ -385,4 +585,6 @@ int main(int argc, char** argv) {
             }
         }
     }
+
+    endGame();
 }
