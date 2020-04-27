76a3d93b0a11a762c28f9c2146bf9afa7d973673
staamp <nicoce25@hotmail.fr>
2020-04-27 17:01:56 +0200

    fct random qui retourne une position pour un pion sur une case libre

--- ia/src/main/java/org/example/Prolog.java
+++ ia/src/main/java/org/example/Prolog.java
@@ -5,51 +5,89 @@ import org.jpl7.Query;
 import org.jpl7.Term;
 import org.jpl7.Variable;
 
-
+/**
+ * This class is the class who interact with artificial intelligence.
+ * The class using prolog file in directory resources.
+ */
 public class Prolog {
 
+    /**
+     * The constructor of the class
+     */
     Prolog() {
 
     }
 
-    public static Query consult() {
+    /**
+     * This function realize the connection with the prolog file
+     *
+     * @return the query to make request to the prolog file
+     */
+    public Query consult() {
         String file = ClassLoader.getSystemResource("ia.pl").getPath();
         Query q1 = new Query("consult", new Term[] {new Atom(file)});
         System.out.println("Consult prolog file : " + (q1.hasSolution() ? "succeeded" : "failed")); //Si le fichier a pu etre lu
         return q1;
     }
 
+    /**
+     * This function return a random integer in the interval 0 and 4
+     *
+     * @return the random integer
+     */
     public int random() {
         Query q1 = consult();
-
-
-        String strq = "random(0,4,S)"; //REQUETE vers le fichier ia: ici un int random entre 0 et 4
-
+        String strq = "random(0,4,S)";
         q1 = new Query(strq);
 
         java.util.Map<String,Term> solution;
         solution = q1.oneSolution();
-        System.out.println("random is " + ( q1.hasSolution() ? "provable" : "not provable")); //Si la solution est possible
-        System.out.println( "S = " + solution.get("S")); //Le resultat
 
         return solution.get("S").intValue();
     }
+
+    /**
+     * This function find the next move. She places a pawn in a random case in the grid and check if the case free
+     *
+     * @param g the Quantik grid with the move of the two players
+     * @return the next move chose by the IA
+     */
+    public int[] jouerCoupRandom(Grille g) {
+
+        int[] coup = {2,0,3};
+        String cpStr = "[2,0,3]";
+
+        Query q1 = consult();
+        String strq = "jouerCoupRandomSurCaseVide(["+cpStr+"],L,C,P).";
+        q1 = new Query(strq);
+
+        java.util.Map<String,Term> solution;
+        solution = q1.oneSolution();
+        System.out.println( "L = " + solution.get("L"));
+        System.out.println( "C = " + solution.get("C"));
+        System.out.println( "P = " + solution.get("P"));
+
+
+        int[] res = new int[3];
+        res[0] = solution.get("L").intValue();
+        res[1] = solution.get("C").intValue();
+        res[2] = solution.get("P").intValue();
+
+        return res;
+    }
+
 
 
 
     public static void main( String[] args ) {
 
-        Query q1 = consult();
+        Prolog p = new Prolog();
 
+        Grille g = new Grille();
+        g.addPawnInGrille(2,0,3);
 
-        String strq = "random(0,4,S)"; //REQUETE vers le fichier ia: ici un int random entre 0 et 4
+        p.jouerCoupRandom(g);
 
-        q1 = new Query(strq);
-
-        java.util.Map<String,Term> solution;
-        solution = q1.oneSolution();
-        System.out.println("random is " + ( q1.hasSolution() ? "provable" : "not provable")); //Si la solution est possible
-        System.out.println( "S = " + solution.get("S")); //Le resultat
-
+        System.out.println("S : " + p.random());
     }
 }
--- ia/src/main/resources/ia.pl
+++ ia/src/main/resources/ia.pl
@@ -1,11 +1,9 @@
 % prolog ia
 
 
-grid = [[L1C1, L1C2, L1C3, L1C4],
-    [L2C1, L2C2, L2C3, L2C4],
-    [L3C1, L3C2, L3C3, L3C4],
-    [L4C1, L4C2, L4C3, L4C4]
-]
+pion(cylindre, pave, sphere, tetraedre).
+pionN(cn, pn, sn, tn).
+pionB(cb, pb, sb, tb).
 
 
 random(L, U, R) :-
@@ -19,3 +17,31 @@ random(L, U, R) :-
 random(L, U, _) :-
     must_be(number, L),
     must_be(number, U).
+
+jouerCoupRandom(L, C, P) :-
+    random(0,4,C),
+    random(0,4,L),
+    random(0,4,P).
+
+jouerCoupRandomSurCaseVide(H, L,C,P) :-
+    random(0,4,L),
+    random(0,4,C),
+    random(0,4,P),
+    COUP = [L,C,P],
+    \+member(COUP, H).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
