c788009b89908bba1e5ecf768f8776c565cb805d
staamp <nicoce25@hotmail.fr>
2020-04-28 17:57:01 +0200

    fct prolog verifLigne + pion dans case

--- ia/src/main/resources/ia.pl
+++ ia/src/main/resources/ia.pl
@@ -5,6 +5,8 @@ pion(cylindre, pave, sphere, tetraedre).
 pionN(cn, pn, sn, tn).
 pionB(cb, pb, sb, tb).
 
+%PT is [cn,pn,sn,tn].
+
 
 random(L, U, R) :-
     integer(L), integer(U),
@@ -31,14 +33,30 @@ jouerCoupRandomSurCaseVide(H, L,C,P) :-
     \+member(COUP, H).
 
 
+% This function check the value of the case
+%
+% Grid : the grid of Quantik game with preceding move of player
+% Ligne : the coordinate in line
+% Colonne : the coordinate in column
+% Val : the value return in case
+retournePionDansCase(Grid, Ligne, Colonne, Val) :-
+    nth0(Ligne,Grid,L),
+    nth0(Colonne,L,Val).
+
+% This function check if the line contain a pawn
+%
+% Grid : the grid of Quantik game with preceding move of player
+% Ligne : the line to check
+% Pion : the pawn to check if is in line
+verifLigne(Grid, Ligne, Pion) :-
+    nth0(Ligne,Grid,L),
+    nth0(_,L,Pion),!.
 
 
 
-
-
-
-
-
+test(T,Val) :-
+    PT = [1,2,3],
+    write(PT).
 
 
 
