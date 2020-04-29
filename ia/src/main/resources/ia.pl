0218296ab823952c5266204fa60c29319697366d
staamp <nicoce25@hotmail.fr>
2020-04-29 14:06:09 +0200

    fonction de detection + TU

--- ia/src/main/resources/ia.pl
+++ ia/src/main/resources/ia.pl
@@ -11,9 +11,12 @@ pion(cylindre, pave, sphere, tetraedre).
 pionN(cn, pn, sn, tn).
 pionB(cb, pb, sb, tb).
 
-%PT is [cn,pn,sn,tn].
 
-
+% This function return a random integer in born
+%
+% L : The min for the draw
+% U : The max for the draw
+% R : return the integer
 random(L, U, R) :-
     integer(L), integer(U),
     !,
@@ -26,17 +29,67 @@ random(L, U, _) :-
     must_be(number, L),
     must_be(number, U).
 
-jouerCoupRandom(L, C, P) :-
-    random(0,4,C),
-    random(0,4,L),
-    random(0,4,P).
 
-jouerCoupRandomSurCaseVide(H, L,C,P) :-
+% This function play a random move in the grid
+%
+% Ligne : return the number of the line played
+% Colonne : return the number of the column played
+% Pion : return the number of the pawn played
+jouerCoupRandom(Ligne, Colonne, Pion) :-
+    random(0,4,Colonne),
+    random(0,4,Ligne),
+    random(0,4,Pion).
+
+
+% This function play a random move in the grid
+%
+% Grid : the grid of Quantik game with preceding move of player
+% Ligne : return the number of the line played
+% Colonne : return the number of the column played
+% Pion : return the pawn played
+% PionRestant : The pawns aviable to play
+jouerCoupRandomSurCaseVide(Grid,Ligne,Colonne,Pion,PionRestant) :-
     random(0,4,L),
     random(0,4,C),
     random(0,4,P),
-    COUP = [L,C,P],
-    \+member(COUP, H).
+    retournePionDansCase(Grid,L,C,Val),
+    Val == 0,
+        Ligne = L,
+        Colonne = C,
+        Pion = P,!;
+        jouerCoupRandomSurCaseVide(Grid,Ligne,Colonne,Pion,PionRestant).
+
+
+
+jouerCoupRandomSurCaseVideT(Grid,Ligne,Colonne,Pion,PionRestant) :-
+    random(0,4,L),
+    random(0,4,C),
+    length(PionRestant,S),
+    S1 is S - 1,
+    random(0,S1,P1),
+    write(P1),
+    nth0(P1,PionRestant,Pion),
+    write(" Pion = "),write(Pion),write("\n"),
+    retournePionDansCase(Grid,L,C,Val),
+    writeln(Val),
+    writeln(L),
+    writeln(C),
+    Val == 0,
+        Ligne = L,
+        Colonne = C,
+        write(Pion),!;
+        write("ici"),
+        jouerCoupRandomSurCaseVide(Grid,Ligne,Colonne,Pion,PionRestant).
+
+
+
+
+
+
+
+
+
+
 
 
 % This function check the value of the case
@@ -47,7 +100,7 @@ jouerCoupRandomSurCaseVide(H, L,C,P) :-
 % Val : the value return in case
 retournePionDansCase(Grid, Ligne, Colonne, Val) :-
     nth0(Ligne,Grid,L),
-    nth0(Colonne,L,Val),write(Val).
+    nth0(Colonne,L,Val).
 
 % This function check if the line contain a pawn
 %
@@ -130,24 +183,37 @@ verifCarre4(Grid, Pion) :-
     Val == Pion,!;
     false.
 
-%Ca marche pas encore, je check demain
+% This function check if the superior rectangle contain a pawn
+%
+% Grid : the grid of Quantik game with the preceding move of player
+% Colonne : the column to check
+% Pion : the pawn to check if is in the square with the coordinate line and column
+verifRectangleSup(Grid, Colonne, Pion) :-
+    Colonne < 2,
+        verifCarre1(Grid,Pion),!;
+        verifCarre2(Grid,Pion),!.
+
+% This function check if the inferior rectangle contain a pawn
+%
+% Grid : the grid of Quantik game with the preceding move of player
+% Colonne : the column to check
+% Pion : the pawn to check if is in the square with the coordinate line and column
+verifRectangleInf(Grid, Colonne, Pion) :-
+    Colonne < 2,
+        verifCarre3(Grid,Pion),!;
+        verifCarre4(Grid,Pion),!.
+
+% This function check if the square determinate with line and column contain a pawn
+%
+% Grid : the grid of Quantik game with the preceding move of player
+% Ligne : the line position of the new move
+% Colonne : the column position of the new move
+% Pion : the pawn to check if is in the square with the coordinate line and column
 verifCarre(Grid, Ligne, Colonne, Pion) :-
-    Ligne < 2 -> Colonne < 2 -> write("00"),
+    Ligne < 2,
+    verifRectangleSup(Grid,Colonne,Pion),!;
+    verifRectangleInf(Grid,Colonne,Pion),!.
 
-    Ligne < 2 -> Colonne > 2 -> write("02"),
-
-    Ligne > 2 -> Colonne < 2 -> write("20"),
-
-    Ligne > 2 -> Colonne > 2 -> write("22").
-
-
-    %Colonne < 2 ,
-    %write("00 11"),!;
-    %write("02 13"),!
-    %;
-    %Colonne < 2 ,
-    %write("20 13"),!;
-    %write("22 33"),!.
 
 
 % Les tests unitaires :
@@ -175,14 +241,49 @@ verifCarre(Grid, Ligne, Colonne, Pion) :-
     test('verifColonne5', [fail]) :-
         verifColonne([[cb,0,0,0],[0,cn,0,0],[0,cb,0,0],[0,0,0,cb]],0,cn).
 
-    test('verifCarre1', [true]) :-
+    test('verifCarre11', [true]) :-
         verifCarre1([[a,b,0,0],[c,d,0,0],[0,0,0,0],[0,0,0,0]],a).
-    test('verifCarre2', [true]) :-
+    test('verifCarre12', [true]) :-
         verifCarre1([[a,b,0,0],[c,d,0,0],[0,0,0,0],[0,0,0,0]],b).
-    test('verifCarre3', [true]) :-
+    test('verifCarre13', [true]) :-
         verifCarre1([[a,b,0,0],[c,d,0,0],[0,0,0,0],[0,0,0,0]],c).
-    test('verifCarre4', [true]) :-
+    test('verifCarre14', [true]) :-
         verifCarre1([[a,b,0,0],[c,d,0,0],[0,0,0,0],[0,0,0,0]],d).
+    test('verifCarre15', [fail]) :-
+        verifCarre1([[a,b,0,0],[c,d,0,0],[0,0,0,0],[0,0,0,0]],0).
+
+    test('verifCarre21', [true]) :-
+        verifCarre2([[0,0,a,b],[0,0,c,d],[0,0,0,0],[0,0,0,0]],a).
+    test('verifCarre22', [true]) :-
+        verifCarre2([[0,0,a,b],[0,0,c,d],[0,0,0,0],[0,0,0,0]],b).
+    test('verifCarre23', [true]) :-
+        verifCarre2([[0,0,a,b],[0,0,c,d],[0,0,0,0],[0,0,0,0]],c).
+    test('verifCarre24', [true]) :-
+        verifCarre2([[0,0,a,b],[0,0,c,d],[0,0,0,0],[0,0,0,0]],d).
+    test('verifCarre25', [fail]) :-
+        verifCarre2([[0,0,a,b],[0,0,c,d],[0,0,0,0],[0,0,0,0]],0).
+
+    test('verifCarre31', [true]) :-
+        verifCarre3([[0,0,0,0],[0,0,0,0],[a,b,0,0],[c,d,0,0]],a).
+    test('verifCarre32', [true]) :-
+        verifCarre3([[0,0,0,0],[0,0,0,0],[a,b,0,0],[c,d,0,0]],b).
+    test('verifCarre33', [true]) :-
+        verifCarre3([[0,0,0,0],[0,0,0,0],[a,b,0,0],[c,d,0,0]],c).
+    test('verifCarre34', [true]) :-
+        verifCarre3([[0,0,0,0],[0,0,0,0],[a,b,0,0],[c,d,0,0]],d).
+    test('verifCarre35', [fail]) :-
+        verifCarre3([[0,0,0,0],[0,0,0,0],[a,b,0,0],[c,d,0,0]],0).
+
+    test('verifCarre41', [true]) :-
+        verifCarre4([[0,0,0,0],[0,0,0,0],[0,0,a,b],[0,0,c,d]],a).
+    test('verifCarre42', [true]) :-
+        verifCarre4([[0,0,0,0],[0,0,0,0],[0,0,a,b],[0,0,c,d]],b).
+    test('verifCarre43', [true]) :-
+        verifCarre4([[0,0,0,0],[0,0,0,0],[0,0,a,b],[0,0,c,d]],c).
+    test('verifCarre44', [true]) :-
+        verifCarre4([[0,0,0,0],[0,0,0,0],[0,0,a,b],[0,0,c,d]],d).
+    test('verifCarre45', [fail]) :-
+        verifCarre4([[0,0,0,0],[0,0,0,0],[0,0,a,b],[0,0,c,d]],0).
 
 :-end_tests(chp0).
 
